8e161554a00712d4bbfffb55d0e6a29ff8dd90b9
Rasim-1 <husanbaevrasim315@gmail.com>
2024-11-03 10:08:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const swiperAlbum2 = new Swiper('.swiper-album-2', {
 
 
 
-const swiperAlbum3 = new Swiper('.swiper-album-2', {
+const swiperAlbum3 = new Swiper('.swiper-album-3', {
     loop: true,
     slidesPerView: 0.3,
     spaceBetween: 8,
@@ -97,5 +97,35 @@ const swiperAlbum3 = new Swiper('.swiper-album-2', {
     allowTouchMove: false,
 
     speed: 20000
+});
+
+const swiperAlbum4 = new Swiper('.swiper-album-4', {
+    loop: true,
+    slidesPerView: 0.3,
+    spaceBetween: 4,
+
+    autoplay: {
+        delay: 0,
+        reverseDirection: true
+
+    },
+
+    breakpoints: {
+        500: {
+            slidesPerView: 0.4,
+        },
+
+        700: {
+            slidesPerView: 0.5,
+        },
+
+        800: {
+            slidesPerView: 0.6,
+        }
+    },
+
+    allowTouchMove: false,
+
+    speed: 30000
 });
 
