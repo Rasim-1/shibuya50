c508f192c0df5291e8bb701096b08311504a8fbf
Rasim-1 <husanbaevrasim315@gmail.com>
2024-12-21 14:47:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -78,6 +78,7 @@ const swiperAlbum3 = new Swiper('.swiper-album-3', {
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
+        reverseDirection: true
     },
 
     breakpoints: {
@@ -106,6 +107,7 @@ const swiperAlbum4 = new Swiper('.swiper-album-4', {
 
     autoplay: {
         delay: 0,
+        disableOnInteraction: false,
         reverseDirection: true
 
     },
@@ -129,3 +131,61 @@ const swiperAlbum4 = new Swiper('.swiper-album-4', {
     speed: 30000
 });
 
+
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+document.querySelector('.prev').addEventListener('click', () => {
+  changeSlide(-1);
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  changeSlide(1);
+});
+
+function changeSlide(step) {
+  slideIndex = (slideIndex + step + totalSlides) % totalSlides;
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${-100 * slideIndex}%)`;
+  });
+}
+
+
+setInterval(() => {
+  changeSlide(1);
+}, 5000);
+
+
+
+// const swiperAlbum5 = new Swiper('.swiper-album-5', {
+//     loop: true,
+//     slidesPerView: 0.9,
+//     spaceBetween: 6,
+
+//     autoplay: {
+//         delay: 0,
+//         disableOnInteraction: false,
+//         reverseDirection: true
+
+//     },
+
+//     breakpoints: {
+//         500: {
+//             slidesPerView: 0.4,
+//         },
+
+//         700: {
+//             slidesPerView: 0.5,
+//         },
+
+//         800: {
+//             slidesPerView: 0.6,
+//         }
+//     },
+
+//     allowTouchMove: false,
+
+//     speed: 10000
+// });
